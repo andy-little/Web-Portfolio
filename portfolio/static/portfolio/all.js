20792c9d5965181e90bac600224f25c6c0cedfb4
andy-little <69594249+andy-little@users.noreply.github.com>
2021-09-14 19:20:45 +0100

    disabled auto slide when lower than projects

--- portfolio/static/portfolio/all.js
+++ portfolio/static/portfolio/all.js
@@ -1,5 +1,63 @@
 import {data} from './data.js'
 
+const titleEl = document.querySelector('.project-title-text');
+const subtitleEl = document.querySelector('.project-subtitle');
+const textEl = document.querySelector('.project-text');
+const imgEl = document.querySelector('.project-laptop');
+const btn = document.querySelector('.site');
+const codeBtn = document.querySelector('.code');
+const animatedItems = document.querySelectorAll('.move');
+let index = 0;
+let interval;
+const slideDuration = 7500;
+
+// toggle through projects
+// set data
+function loadProject(){
+    const {code, img, site:url, subtitle, text, title} = data[index];
+    imgEl.src = img;
+    btn.href = url;
+    codeBtn.href = code;
+    titleEl.textContent = title;
+    titleEl.href = url;
+    subtitleEl.textContent = subtitle;
+    textEl.innerHTML = text;
+}
+//animate project change
+function changeProject(direction){
+    clearInterval(interval);
+    let step1 = 'translateX(100vw)';
+    let step2 = 'translateX(-100vw)';
+    if(direction === 'prev'){
+        index--;
+        if (index < 0) {
+            index = data.length - 1;
+        }
+    }else{
+        //swaps values so that animation is reversed
+        [step1, step2] =[step2, step1];
+        index++;
+        if(index >= data.length){
+            index = 0;
+        }
+    }
+    animatedItems.forEach((item)=>{
+        item.style.transform =  step1;
+        setTimeout(()=>{
+            loadProject();
+            item.style.opacity =  0;
+            item.style.transform =  step2;
+        },100);
+        
+        setTimeout(()=>{
+            item.style.opacity =  1;
+            item.style.transform = 'translateX(0vw)';
+        },200);
+        
+    });
+    interval = setInterval(changeProject, slideDuration);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
     //Toggle between hamburger and close symbol
@@ -23,130 +81,46 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
     
-    const titleEl = document.querySelector('.project-title-text');
-    const subtitleEl = document.querySelector('.project-subtitle');
-    const textEl = document.querySelector('.project-text');
-    const imgEl = document.querySelector('.project-laptop');
-    const btn = document.querySelector('.site');
-    const codeBtn = document.querySelector('.code');
-    const animatedItems = document.querySelectorAll('.move');
-    let index = 0;
-    let interval;
-    const slideDuration = 7500;
-
-    
-    
-    function loadProject(){
-        const {code, img, site:url, subtitle, text, title} = data[index];
-        imgEl.src = img;
-        btn.href = url;
-        codeBtn.href = code;
-        titleEl.textContent = title;
-        titleEl.href = url;
-        subtitleEl.textContent = subtitle;
-        textEl.innerHTML = text;
-    }
-    function changeProject(direction){
-        clearInterval(interval);
-        let step1 = 'translateX(100vw)';
-        let step2 = 'translateX(-100vw)';
-        if(direction === 'prev'){
-            index--;
-            if (index < 0) {
-                index = data.length - 1;
-            }
-        }else{
-            //swaps values so that animation is reversed
-            [step1, step2] =[step2, step1];
-            index++;
-            if(index >= data.length){
-                index = 0;
-            }
-        }
-        animatedItems.forEach((item)=>{
-            item.style.transform =  step1;
-            setTimeout(()=>{
-                loadProject();
-                item.style.opacity =  0;
-                item.style.transform =  step2;
-            },100);
-            
-            setTimeout(()=>{
-                item.style.opacity =  1;
-                item.style.transform = 'translateX(0vw)';
-            },200);
-            
-        });
-        interval = setInterval(changeProject, slideDuration);
-        
-    }
     const nextProject = document.querySelector('.next-project');
     const prevProject = document.querySelector('.prev-project')
     nextProject.onclick = () => changeProject('next');
     prevProject.onclick = () => changeProject('prev');
     interval = setInterval(changeProject, slideDuration);
-    
-    
+
 });
 
-window.onload = () => {
-    
-    //Replace desktop menu with minimize sign when scrolling past header 
-    const deskMenu = document.querySelector('.desk-menu');
-    
-    
-    
-    window.addEventListener('scroll', () => {
-        //AND hide minimise sign and menu when at contact section
-        const contact = document.querySelector('#contact');
-        const contactPosition = contact.offsetTop - 250;
-        //hide menu and show
-        if (window.pageYOffset > 315 && deskMenu.classList.contains('open')) {
-            deskMenu.classList.remove('open')
-        } else if (window.pageYOffset < 315 && !deskMenu.classList.contains('open')) {
-            deskMenu.classList.add('open');
-        } else if (window.pageYOffset > contactPosition) {
-            //hide minimise sign and show
-            deskMenu.classList.add('close');
-        } else if (window.pageYOffset < contactPosition) {
-            deskMenu.classList.remove('close');
-        }
+    window.onload = () => {
         
+        //Replace desktop menu with minimize sign when scrolling past header 
+        const deskMenu = document.querySelector('.desk-menu');
+        
+        
+        
+        window.addEventListener('scroll', () => {
+            //AND hide minimise sign and menu when at contact section
+            const contact = document.querySelector('#contact');
+            const contactPosition = contact.offsetTop - 250;
+            //hide menu and show
+            if (window.pageYOffset > 315 && deskMenu.classList.contains('open')) {
+                deskMenu.classList.remove('open')
+            } else if (window.pageYOffset < 315 && !deskMenu.classList.contains('open')) {
+                deskMenu.classList.add('open');
+            } else if (window.pageYOffset > contactPosition) {
+                //hide minimise sign and show
+                deskMenu.classList.add('close');
+            } else if (window.pageYOffset < contactPosition) {
+                deskMenu.classList.remove('close');
+            }
+
+        // turn off project auto scroll when lower on page
+        // this prevents the page resizing and jumping
+            let imgBottom = imgEl.offsetTop + imgEl.getBoundingClientRect().height;
+            if(window.pageYOffset > imgBottom - 20 && interval !== 0){
+                clearInterval(interval);
+                interval = 0;
+                
+            } else if(window.pageYOffset < imgBottom - 20 && interval === 0){
+                interval = setInterval(changeProject, slideDuration);
+        }
     });
 };
-
-/*     function loadProject(project) {
-        let csrftoken = getCookie('csrftoken');
-        fetch(`/project/${project}`, {
-            method: 'GET',
-            headers: { "X-CSRFToken": csrftoken },
-        })
-        .then(response => response.json())
-        .then(result => {
-            //do something with object here
-            console.log(result)
-        }).catch(error => console.log(error));
-        
-    } */
-
-
-
-
-/* 
-
-// handle csrf token for fetch()
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        let cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            let cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-} */
